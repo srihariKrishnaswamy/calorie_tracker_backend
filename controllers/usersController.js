f389c2beed47296156f7dd4ff1953d9d69705694
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-09-18 23:16:32 -0700

    update user route updated

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -29,11 +29,11 @@ export const getUser = asyncHandler(async (req, res) => {
 })
 
 export const createUser = asyncHandler (async (req, res) => {
-    const {email, password, first_name, last_name, birth_day, sex, weight, height, target_calories} = req.body;
+    const {email, password, first_name, last_name, birth_day, sex, weight, height, target_calories, timezone} = req.body;
     const [result] = await pool.query(`
-    INSERT INTO users (email, hashedPW, first_name, last_name, birth_day, sex, weight, height, target_calories)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `, [email, password, first_name, last_name, birth_day, sex, weight, height, target_calories])
+    INSERT INTO users (email, hashedPW, first_name, last_name, birth_day, sex, weight, height, target_calories, timezone)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `, [email, password, first_name, last_name, birth_day, sex, weight, height, target_calories, timezone])
     const id = result.insertId
     const [rows] = await pool.query(`
     SELECT *
@@ -42,4 +42,30 @@ export const createUser = asyncHandler (async (req, res) => {
     `, [id])
     console.log("new user created")
     res.status(201).json(rows[0])
+})
+
+export const updateUser = asyncHandler (async (req, res) => {
+    const {user_id, password, sex, weight, height, target_calories, timezone, current_daily_total_id, pinned_user_1_id, pinned_user_2_id, pinned_user_3_id, pinned_user_4_id, pinned_user_5_id} = req.body;
+    const [result] = await pool.query(`
+    UPDATE users
+    SET hashedPW = ?, sex = ?, weight = ?, height = ?, target_calories = ?, timezone = ?, current_daily_total_id = ?, pinned_user_1_id = ?, pinned_user_2_id = ?, pinned_user_3_id = ?, pinned_user_4_id = ?, pinned_user_5_id = ?
+    WHERE user_id = ?
+    `, [password, sex, weight, height, target_calories, timezone, current_daily_total_id, pinned_user_1_id, pinned_user_2_id, pinned_user_3_id, pinned_user_4_id, pinned_user_5_id, user_id])
+    const [rows] = await pool.query(`
+    SELECT *
+    FROM users
+    WHERE user_id = ?
+    `, [user_id])
+    console.log("user updated")
+    res.status(201).json(rows[0])
+})
+
+export const deleteUser = asyncHandler (async (req, res) => {
+    const {user_id} = req.body;
+    await pool.query(`
+    DELETE FROM users
+    WHERE user_id = ?
+    `, [user_id])
+    console.log("user deleted")
+    res.status(201).json({message: "user deleted"})
 })
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import  { getUsers, getUser, createUser } from '../controllers/usersController.js';
+import  { updateUser, deleteUser, getUsers, getUser, createUser } from '../controllers/usersController.js';
 
 const router = express.Router();
 
 router.route('/')
     .post(createUser)
     .get(getUsers)
+    .delete(deleteUser)
+    .patch(updateUser)
 
 router.route('/:id').get(getUser)
 
