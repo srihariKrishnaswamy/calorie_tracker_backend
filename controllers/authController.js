41dc3728af3c697eaaf99681d30dac05b59859a7
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-10-02 21:59:50 -0700

    working out a bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,6 +21,7 @@ const pool = mysql.createPool({
 // @access Public
 const login = asyncHandler(async (req, res) => {
     const {email, password} = req.body
+    console.log(email, password)
     if(!email || !password) {
         return res.status(400).json({message: 'All fields are required'})
     }
