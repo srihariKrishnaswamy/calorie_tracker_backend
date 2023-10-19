cd043bed81b18282120913096fb3261a6d4c5ede
Srihari Krishnaswamy <86600946+srihariKrishnaswamy@users.noreply.github.com>
2023-10-18 19:15:50 -0700

    Update entriesRoutes.js

--- routes/entriesRoutes.js
+++ routes/entriesRoutes.js
@@ -5,7 +5,7 @@ import verifyJWT from '../middleware/verifyJWT.js'
 const router = express.Router();
 router.use(verifyJWT)
 
-router.route('/all/').get(getAllEntries) // only for development
+// router.route('/all/').get(getAllEntries) // only for development
 
 router.route('/')
     .post(createEntry)
